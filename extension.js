853ee319b4ac2bd4680ecf19ed1efd13b3531a6e
Jeremy Soller <jeremy@system76.com>
2021-01-13 13:00:55 -0700

    Integrate Pop Shop details

--- extension.js
+++ extension.js
@@ -1,6 +1,9 @@
+const AppDisplay = imports.ui.appDisplay;
 const AltTab = imports.ui.altTab;
 const Main = imports.ui.main;
+const Shell = imports.gi.Shell;
 const SwitcherPopup = imports.ui.switcherPopup;
+const Util = imports.misc.util;
 
 let injections = [];
 
@@ -13,25 +16,43 @@ function inject(object, parameter, replacement) {
     object[parameter] = replacement;
 }
 
-function alt_tab_finish(timestamp) {
-    let appIcon = this._items[this._selectedIndex];
-    if (this._currentWindow < 0)
-        Main.activateWindow(appIcon.cachedWindows[0], timestamp);
-    else
-        Main.activateWindow(appIcon.cachedWindows[this._currentWindow], timestamp);
-
-    SwitcherPopup.SwitcherPopup.prototype._finish.apply(this, [timestamp]);
-}
-
 function init(metadata) {}
 
 function enable() {
     // Raise first window on alt-tab
-    inject(AltTab.AppSwitcherPopup.prototype, "_finish", alt_tab_finish);
+    inject(AltTab.AppSwitcherPopup.prototype, "_finish", function() {
+        let appIcon = this._items[this._selectedIndex];
+        if (this._currentWindow < 0)
+            Main.activateWindow(appIcon.cachedWindows[0], timestamp);
+        else
+            Main.activateWindow(appIcon.cachedWindows[this._currentWindow], timestamp);
+
+        SwitcherPopup.SwitcherPopup.prototype._finish.apply(this, [timestamp]);
+    });
 
     // Always show workspaces picker
     inject(Main.overview._overview._controls._thumbnailsSlider, "_getAlwaysZoomOut", function() {
         return true;
+    });
+
+    // Pop Shop details
+    let original_rebuildMenu = AppDisplay.AppIconMenu.prototype._rebuildMenu;
+    inject(AppDisplay.AppIconMenu.prototype, "_rebuildMenu", function() {
+        let ret = original_rebuildMenu.apply(this, arguments);
+
+        if (!this._source.app.is_window_backed()) {
+            if (Shell.AppSystem.get_default().lookup_app('io.elementary.appcenter.desktop')) {
+                this._appendSeparator();
+                let item = this._appendMenuItem(_("Show Details"));
+                item.connect('activate', () => {
+                    let id = this._source.app.get_id();
+                    Util.trySpawn(["io.elementary.appcenter", "appstream://" + id]);
+                    Main.overview.hide();
+                });
+            }
+        }
+
+        return ret;
     });
 }
 
