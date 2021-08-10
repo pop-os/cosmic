050b76543d11640dbde3a36d83cef28963dbd978
John-Mark Allen <john-mark@qu-2.com>
2021-08-10 21:57:59 +0100

    Prefs switch responds to settings change elsewhere

--- prefs.js
+++ prefs.js
@@ -9,6 +9,8 @@ var { settings_new_schema } = extension.imports.settings;
 
 let settings = null;
 
+const DISABLE_SUPER_SETTINGS_KEY = "disable-overlay-key";
+
 function open_panel() {
     const appinfo = Gio.DesktopAppInfo.new("gnome-background-panel.desktop");
     const launch_ctx = Gdk.Display.get_default().get_app_launch_context();
@@ -27,9 +29,14 @@ function buildPrefsWidget() {
     settings_button.connect("clicked", open_panel);
 
     const disable_super_switch = ui.get_object("switch-disable-super");
-    disable_super_switch.set_state(settings.get_boolean("disable-overlay-key"));
+    disable_super_switch.set_state(settings.get_boolean(DISABLE_SUPER_SETTINGS_KEY));
     disable_super_switch.connect("notify::active", (widget) => {
-        settings.set_boolean("disable-overlay-key", widget.active);
+        settings.set_boolean(DISABLE_SUPER_SETTINGS_KEY, widget.active);
+    });
+    settings.connect("changed", (settings, key) => {
+        if (key === DISABLE_SUPER_SETTINGS_KEY) {
+            disable_super_switch.set_state(settings.get_boolean(key));
+        }
     });
 
     return ui.get_object("main-widget");
